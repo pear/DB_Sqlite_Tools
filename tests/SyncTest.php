442f7a16b60337afd26ae8f0b4f4d9e430ee9bcf
David Costa <gurugeek@php.net>
2004-08-21 02:15:42 +0000

    adding synctest
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Sqlite_Tools/trunk@166937 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/SyncTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'Tools.php';
+
+// defining one or more databases in our array 
+$databases = array ('blogs.sqlite');  
+
+$sqlite = new DB_Sqlite_Tools ($databases);
+
+// changing the sync value to FULL for one or more databases 
+$sqlite->sync('FULL');
+
+/* output 
+
+
+<logevent>
+   <class>DB_Sqlite_Tools</class>
+   <function>sync</function>
+   <data>
+'Synchronous value correctly reset to FULL'
+   </data>
+</logevent>
+
+
+/*
+?>
+
