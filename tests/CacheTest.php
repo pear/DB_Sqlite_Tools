a114aff3b4db65bde9516aa683f4bcaf0b859422
David Costa <gurugeek@php.net>
2004-08-28 23:28:50 +0000

    sync with tools changes
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Sqlite_Tools/trunk@167522 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -10,22 +10,12 @@ $sqlite = new DB_Sqlite_Tools ($databases);
 $sqlite->showLogs = true ;
 // we increase the Cache value to 10000
 $sqlite->CacheSize(10000);
-// and exect the following output on the browser and optionally on our log db
-
-/*
-
-<logevent>
-   <class>DB_Sqlite_Tools</class>
-   <function>CacheSize</function>
-   <data>
-'cache correctly reset to 10000'
-   </data>
-</logevent>
-
-
-
-*/ 
-
-
+// and add the event to a log db logs.sqlite
+// note that on the second parameter we specify
+// true for the table creation and eventually the table
+// name mylogs. This will work the first time with the table created, once we have a table
+// we will call it as $sqlite->sqliteLogs('logs.sqlite.',false,'mylogs');
+$sqlite->sqliteLogs('logs.sqlite.',true,'mylogs');
+ 
 ?>
 
