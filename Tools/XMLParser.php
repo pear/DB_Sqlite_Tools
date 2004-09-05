61167c903b5664cbafbac3f2d9cddc5763c213e3
David Costa <gurugeek@php.net>
2004-09-05 16:40:55 +0000

    Fixed indenting, CS.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Sqlite_Tools/trunk@168030 c90b9560-bf6c-de11-be94-00142212c4b1

--- Tools/XMLParser.php
+++ Tools/XMLParser.php
@@ -26,112 +26,77 @@
 // Stephan Schimdt 
 
 class DB_Sqlite_Tools_XMLParser {
-  public $fh;
-  public $element;
-  public $enclosed;
-  public $ignoreList = array();
-
-  public function __construct( $fh, $pos = 0 ) 
-  {
-    $this->fh = $fh ;
-    fseek( $fh, $pos ) ;
-    $this->strlen = strlen( $this->str ) ;
-  }
-
-  public function getNextElement() 
-  {
-    // the loop is so that if we are told to ignore a certain
-    // element, then we can continue on to the next one and
-    // return that.
-    while(true) {
-
-      // obviously, if we're at EOF, then there are no more
-      // elements ;)
-      if( feof($this->fh) ) return false ;
-
-      // read up to the first open bracket, storing what's
-      // in between
-
-      $this->enclosed = "" ;
-      $c = fgetc( $this->fh ) ;
-      
-      while( ($c != '<') && !feof($this->fh) )
-      {
-        $this->enclosed .= $c ;
-	$c = fgetc( $this->fh ) ;
-      }
-
-      // read up to the first close bracket that isn't within
-      // quote marks, storing what's in between.
-      
-      $this->element = "" ;
-      $inQuote = false ;
-      $c = fgetc($this->fh) ;
-
-      while(
-           ($c != '>')   // end if $c == '>'
-	|| ($inQuote)    // unless this is within a quote,
-	|| (feof($this->fh))   // or if we have reached EOF.
-	   ) {
-        
-	// toggle quote flag
-        if( $c == '"' ) $inQuote = !$inQuote ;
-
-	$this->element .= $c ;
-	$c = fgetc($this->fh) ;
-      }
-
-      // default action is to accept this element, however we have
-      // to check it against the list of elements to ignore, like 
-      // <!-- -->
-      
-      $break = true ;
-
-      foreach( $this->ignoreList as $ignore ) {
-        if( preg_match("/$ignore/", $this->getElement()) )
-	  $break = false ;
-      }
-
-      // break the while loop if this is an acceptable element
-
-      if( $break ) break ;
-
-    } 
-    return true ;
-  }
-
-  public function ignore( $str ) 
-  {
-    // add $str to ignore list
-    
-    $this->ignoreList[] = $str ;  
-  }
-
-  public function getElement() 
-  {
-    return trim( $this->element ) ;
-  }
-
-  public function getEnclosed() 
-  {
-    return $this->enclosed ;
-  }
-
-  public function getElementAttribute( $name ) 
-  {
-    $el = $this->getElement() ;
-
-    preg_match( '/[ ]+'.$name.'[ ]*=[ ]*"([^"]*)"/', $el, $result ) ;
-    return $result[1] ;
-
-  }
-
-  public function getElementName() 
-  {
-    preg_match( "/^([^ ]*).*$/", $this->getElement(), $result ) ;
-    #echo $result[1] ;
-    return $result[1] ;
-  }
-}
-
+        public $fh;
+        public $element;
+        public $enclosed;
+        public $ignoreList = array();
+        public function __construct($fh, $pos = 0) {
+            $this->fh = $fh;
+            fseek($fh, $pos);
+            $this->strlen = strlen($this->str);
+        }
+        public function getNextElement() {
+            // the loop is so that if we are told to ignore a certain
+            // element, then we can continue on to the next one and
+            // return that.
+            while (true) {
+                // obviously, if we're at EOF, then there are no more
+                // elements ;)
+                if (feof($this->fh)) return false;
+                // read up to the first open bracket, storing what's
+                // in between
+                $this->enclosed = "";
+                $c = fgetc($this->fh);
+                while (($c != '<') && !feof($this->fh)) {
+                    $this->enclosed.= $c;
+                    $c = fgetc($this->fh);
+                }
+                // read up to the first close bracket that isn't within
+                // quote marks, storing what's in between.
+                $this->element = "";
+                $inQuote = false;
+                $c = fgetc($this->fh);
+                while (($c != '>') // end if $c == '>'
+                 || ($inQuote) // unless this is within a quote,
+                 || (feof($this->fh)) // or if we have reached EOF.
+                ) {
+                    // toggle quote flag
+                    if ($c == '"') $inQuote = !$inQuote;
+                    $this->element.= $c;
+                    $c = fgetc($this->fh);
+                }
+                // default action is to accept this element, however we have
+                // to check it against the list of elements to ignore, like
+                // <!-- -->
+                $break = true;
+                foreach($this->ignoreList as $ignore) {
+                    if (preg_match("/$ignore/", $this->getElement())) $break = false;
+                }
+                // break the while loop if this is an acceptable element
+                if ($break) break;
+            }
+            return true;
+        }
+        public function ignore($str) {
+            // add $str to ignore list
+            $this->ignoreList[] = $str;
+        }
+        public function getElement() {
+            return trim($this->element);
+        }
+        public function getEnclosed() {
+            return $this->enclosed;
+        }
+        public function getElementAttribute($name) {
+            $el = $this->getElement();
+            preg_match('/[ ]+'.$name.'[ ]*=[ ]*"([^"]*)"/', $el, $result);
+            return $result[1];
+        }
+        public function getElementName() {
+            preg_match("/^([^ ]*).*$/", $this->getElement(), $result);
+            #echo $result[1] ;
+            return $result[1];
+        }
+    }
 ?>
+
