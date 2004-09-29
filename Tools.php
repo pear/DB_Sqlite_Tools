12486e1eee687ad17624982f3da848ea4db84122
David Costa <gurugeek@php.net>
2004-09-29 12:07:30 +0000

    Preparing 0.1.3 release
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Sqlite_Tools/trunk@169531 c90b9560-bf6c-de11-be94-00142212c4b1

--- Tools.php
+++ Tools.php
@@ -1,20 +1,41 @@
 <?php
-
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4  */
 // +----------------------------------------------------------------------+
 // | PHP version 5                                                        |
+// +----------------------------------------------------------------------+/
+// | Copyright (c) 2004 David Costa                                       |                                     // | All rights reserved.                                                 |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
+// |                                                                      |
+// | Redistribution and use in source and binary forms, with or without   |
+// | modification, are permitted provided that the following conditions   |
+// | are met:                                                             |
+// |                                                                      |
+// | Redistributions of source code must retain the above copyright       |
+// | notice, this list of conditions and the following disclaimer.        |
+// |                                                                      |
+// | Redistributions in binary form must reproduce the above copyright    |
+// | notice, this list of conditions and the following disclaimer in the  |
+// | documentation and/or other materials provided with the distribution. |
+// |                                                                      |
+// | Neither the name of David Costa nor the names of his contributors may|
+// | be used to endorse or promote products derived from this software    |
+// | without specific prior written permission.                           |
+// |                                                                      |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
+// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE      |
+// | REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,          |
+// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
+// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS|
+// |  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  |
+// | AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT          |
+// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY|
+// | WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          |
+// | POSSIBILITY OF SUCH DAMAGE.                                          |
 // +----------------------------------------------------------------------+
 // | Authors: David Costa     <gurugeek@php.net>                          |
+// | Authors: Ashley Hewson <morbidness@gmail.com>                        |
 // +----------------------------------------------------------------------+
 //
 // \$Id$  $
@@ -53,12 +74,8 @@
  *
  * @ 
  */
-
     function __autoload($class) {
         switch ($class) {
-            case 'DB_Sqlite_Tools_Xtea':
-                include ('Tools/Xtea.php');
-            break;
             case 'DB_Sqlite_Tools_DBC':
                 include ('Tools/DBC.php');
             break;
@@ -66,11 +83,13 @@
                 include ('Tools/Exception.php');
             break;
             case 'DB_Sqlite_Tools_XMLParser':
-                include ('Tools/XMLParser.php');
+                 include ('Tools/XMLParser.php');
+            break;
+            case 'DB_Sqlite_Tools_ArcFour':
+                include ('Tools/ArcFour.php');
             break;
         }
     }
-    
 class DB_Sqlite_Tools
 {
     /**
@@ -748,8 +767,8 @@ RSYNC;
             $obj = $this->dbobj = new SQLiteDatabase("$db");
         }
         catch(Exception $obj) {
-            echo self::DB_SQLITE_TOOLS_COD.$this->dbobj->getCode() .": ".$this->dbobj->getMessage() ."\n\t";
-            echo "on ".$this->dbobj->getFile() .":".$this->dbobj->getLine() ."\n";
+            echo self::DB_SQLITE_TOOLS_COD.$obj->getCode() .": ".$obj->getMessage() ."\n\t";
+            echo "on ".$obj->getFile() .":".$obj->getLine() ."\n";
             return false ;
         }
         return true;
